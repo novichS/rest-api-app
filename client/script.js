a9b0acb81bcf98deff757f27fa24de0f931471ca
svasyanovych <svitlana.v92@gmail.com>
2020-04-17 02:38:28 +0300

    finished CRUD operation on the back side

--- client/script.js
+++ client/script.js
@@ -1,9 +1,20 @@
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js";
 
+Vue.component("loader", {
+  template: `
+  <div style="display: flex;justify-content: center;align-items: center">
+    <div class="spinner-grow text-success" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+    `
+});
+
 new Vue({
   el: "#app",
   data() {
     return {
+      loading: false,
       form: {
         name: "",
         value: ""
@@ -17,19 +28,41 @@ new Vue({
     }
   },
   methods: {
-    createContact() {
-      const { ...contact } = this.form;
-      this.contacts.push({ ...contact, id: Date.now(), marked: false });
+    async createContact() {
+      const {
+        ...contact
+      } = this.form;
+      const newContact = await request(
+        "http://localhost:3000/api/contacts",
+        "POST",
+        contact
+      );
+      this.contacts.push(newContact);
 
       this.form.name = this.form.value = "";
     },
-    markContact(id) {
+    async markContact(id) {
       const contact = this.contacts.find(contact => contact.id === id);
+      const updated = await request(
+        `http://localhost:3000/api/contacts/${id}`,
+        "PUT", {
+          ...contact
+        }
+      );
       contact.marked = !contact.marked;
     },
-    removeContact(id) {
+    async removeContact(id) {
+      await request(
+        `http://localhost:3000/api/contacts/${id}`,
+        "DELETE"
+        );
       this.contacts = this.contacts.filter(contact => contact.id !== id);
     }
+  },
+  async mounted() {
+    this.loading = true;
+    this.contacts = await request("http://localhost:3000/api/contacts");
+    this.loading = false;
   }
 });
 
@@ -37,6 +70,7 @@ async function request(url, method = "GET", data = null) {
   try {
     const headers = {};
     let body;
+
     if (data) {
       headers["Content-Type"] = "application/json";
       body = JSON.stringify(data);
@@ -47,8 +81,8 @@ async function request(url, method = "GET", data = null) {
       headers,
       body
     });
-    return await response,json()
+    return await response.json();
   } catch (e) {
-    console.log("Error: ", e.message);
+    console.warn("Error:", e.message);
   }
 }
