3265058de84b9a886d0a68997a95ad094d5b5305
svasyanovych <svitlana.v92@gmail.com>
2020-04-16 23:10:27 +0300

    init simple vue cod

--- client/script.js
+++ client/script.js
@@ -1,3 +1,13 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
-console.log(Vue);
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            }
+        }
+    }
+});
