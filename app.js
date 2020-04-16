77f40404e15bcffbd219eaae566703ebbca523c3
svasyanovych <svitlana.v92@gmail.com>
2020-04-16 22:18:16 +0300

    added bootstrap and get request

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+})
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
