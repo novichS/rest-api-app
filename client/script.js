92de82a066fd2734da47670f6dc79cc3708c0136
svasyanovych <svitlana.v92@gmail.com>
2020-04-17 00:00:20 +0300

    added "marked" logic

--- client/script.js
+++ client/script.js
@@ -1,22 +1,31 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js";
 
 new Vue({
-    el: '#app',
-    data() {
-        return {
-            form: {
-                name: '',
-                value: ''
-            },
-            contacts: []
-        }
-    },
-    methods: {
-        createContact() {
-            const {...contact} = this.form;
-            this.contacts.push({...contact, id: Date.now()});
+  el: "#app",
+  data() {
+    return {
+      form: {
+        name: "",
+        value: ""
+      },
+      contacts: [
+          {id: 1, name: 'Svitlana', value: 'developer', marked: false}
+      ]
+    };
+  },
+  methods: {
+    createContact() {
+      const { ...contact } = this.form;
+      this.contacts.push({ ...contact, id: Date.now(), marked: false });
 
-            this.form.name = this.form.value = '';
-        }
+      this.form.name = this.form.value = "";
+    },
+    markContact(id) {
+      const contact = this.contacts.find(contact => contact.id === id);
+      contact.marked = !contact.marked;
+    },
+    removeContact(id) {
+      console.log(id);
     }
+  }
 });
